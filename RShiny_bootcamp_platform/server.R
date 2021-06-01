0caa862276084ec8b732a566e59a158c722129ec
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-05-31 22:13:35 -0400

    added comments to server file

--- RShiny_bootcamp_platform/server.R
+++ RShiny_bootcamp_platform/server.R
@@ -9,14 +9,17 @@ source('gdpGenerate.R')
 
 server <- function(input, output) {
   
+  # reads in data only once to the web application
   dataInput <- reactive({
     getData('data/idb_data.csv')
   })
   
+  # creates and sends updated GDP plot
   output$gdpTimeline <- renderPlot({
     generate_gdp_timeline(dataInput(), input$countries_gdp)
   })
   
+  # generates a list of countries from data sample
   output$countryList <- reactive({
     getCountryList(dataInput())
   })
